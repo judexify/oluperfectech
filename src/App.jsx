8bffcdb591c549ae72ce0dbecff329ed45119943
Judexify <olukojuoluwadunsi12@gmail.com>
2026-05-10 13:40:19 +0100

    Fix Path Issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
-import Faq from "./Sections/FAQ/faq";
+import Faq from "./Sections/FAQ/Faq";
 import Footer from "./Sections/Footer/Footer";
 import Hero from "./Sections/Hero/Hero";
 import HowItWorks from "./Sections/HowItWorks/HowItWorks";
 import Services from "./Sections/OurServices/Services";
 import Pricing from "./Sections/Pricing/Pricing";
-import Form from "./Components/Form/form";
 
 function App() {
   return <AppComponents />;
